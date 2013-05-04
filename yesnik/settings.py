bf73b0c3a4895d6e658ad9bcbea313e7c18c5e6e
yesnik <yesnik@yandex.ru>
2013-05-04 22:30:15 +0600

    Add postgreSQL and fixtures

--- yesnik/settings.py
+++ yesnik/settings.py
@@ -11,6 +11,19 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'yesnik',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+'''
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'yesnik.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
@@ -20,6 +33,7 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default.
     }
 }
+'''
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -56,6 +70,10 @@ MEDIA_ROOT = ''
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
+
+#FIXTURE_DIRS = (
+#    'fixtures',
+#)
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
